9eadfc5b54ece364255b1772b5e6ffc4767058cd
YrayPixels <moseserhinyodavwe2@gmail.com>
2022-08-12 12:01:39 -0700

    edited the printf file

--- _printf.c
+++ _printf.c
@@ -25,7 +25,7 @@ if (format[i] != '%')
 {
 buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_ind);
+print_buffer(buffer, &buff_ind);
 /* write(1, &format[i], 1);*/
 printed_chars++;
 }
